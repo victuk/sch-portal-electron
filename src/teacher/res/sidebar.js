fdba997394f22c1069027a8ba8ebb82c9217c481
Victor <ukokjnr@gmail.com>
2023-08-23 17:49:47 +0100

    changes made

---
+++ src/teacher/res/sidebar.js
@@ -0,0 +1,65 @@
+
+
+document.querySelector(".sidebar-list").innerHTML = `
+<li class="sidebar-list-item home">
+<a href="./index.html">
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="18"
+    height="18"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    class="feather feather-home"
+  >
+    <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
+    <polyline points="9 22 9 12 15 12 15 22" />
+  </svg>
+  <span>Announcements</span>
+</a>
+</li>
+<li class="sidebar-list-item pay-school-fee">
+<a href="./class-position.html">
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="18"
+    height="18"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    class="feather feather-shopping-bag"
+  >
+    <path d="M6 2L3 6v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V6l-3-4z" />
+    <line x1="3" y1="6" x2="21" y2="6" />
+    <path d="M16 10a4 4 0 0 1-8 0" />
+  </svg>
+  <span>Class Student's Result</span>
+</a>
+</li>
+<li class="sidebar-list-item check-result">
+<a href="./loadresult.html">
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="18"
+    height="18"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    class="feather feather-pie-chart"
+  >
+    <path d="M21.21 15.89A10 10 0 1 1 8 2.83" />
+    <path d="M22 12A10 10 0 0 0 12 2v10z" />
+  </svg>
+  <span>Assessment Manager</span>
+</a>
+</li>
+`;
